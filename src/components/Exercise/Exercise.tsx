21e5c0bc4c5b924ef633cff484333998bf9c492a
untypedjay <jay.developes@simplelogin.fr>
2021-10-06 10:17:23 +0200

    Use range inputs for repetitions and weight

--- src/components/Exercise/Exercise.tsx
+++ src/components/Exercise/Exercise.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { ExerciseType } from "../../App";
 import Timer from "../Timer/Timer";
@@ -20,38 +20,14 @@ const StyledExerciseName = styled.h3`
   text-align: center;
 `;
 
-const StyledGridContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 1em;
-`;
-
-const StyledButton = styled.button`
-  color: white;
-  background-color: black;
-  border: none;
-  border-radius: 8px;
-  padding: 16px;
-  font-weight: bold;
-  font-size: 16px;
-`;
-
 interface Props {
   children: ExerciseType;
   triggerNext: () => void;
 }
 
 export default function Exercise({ children, triggerNext }: Props) {
-  const [currentReps, setCurrentReps] = useState("");
-  const [currentWeight, setCurrentWeight] = useState("");
-
-  useEffect(() => {
-    if (currentReps.length && currentWeight.length) {
-      triggerNext();
-      setCurrentReps("");
-      setCurrentWeight("");
-    }
-  }, [currentReps, currentWeight, triggerNext]);
+  const [currentReps, setCurrentReps] = useState(children.suggestedReps);
+  const [currentWeight, setCurrentWeight] = useState(children.suggestedWeight);
 
   return (
     <StyledExercise>
@@ -64,61 +40,16 @@ export default function Exercise({ children, triggerNext }: Props) {
       ) : (
         <>
           <p>
-            {children.suggestedReps} reps
-            {children.suggestedWeight && (
-              <span> / {children.suggestedWeight} kg</span>
+            {currentReps} reps
+            {currentWeight && (
+              <span> / {currentWeight} kg</span>
             )}
           </p>
           <div>Info Card here</div>
-          {currentReps.length === 0 ? (
-            <>
-              <p>Amount of completed repetitions:</p>
-              <StyledGridContainer>
-                <StyledButton onClick={() => setCurrentReps("0 - 5")}>
-                  &lt; 6
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentReps("6 - 10")}>
-                  6 - 10
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentReps("11 - 15")}>
-                  11 - 15
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentReps("16 - 20")}>
-                  16 - 20
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentReps("21 - 25")}>
-                  21 - 25
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentReps("26 - 30")}>
-                  26 - 30
-                </StyledButton>
-              </StyledGridContainer>
-            </>
-          ) : (
-            <>
-              <p>Applied weight:</p>
-              <StyledGridContainer>
-                <StyledButton onClick={() => setCurrentWeight("4.5 kg")}>
-                  4.5 kg
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentWeight("7 kg")}>
-                  7 kg
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentWeight("9.5 kg")}>
-                  9.5 kg
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentWeight("12 kg")}>
-                  12 kg
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentWeight("14.5 kg")}>
-                  14.5 kg
-                </StyledButton>
-                <StyledButton onClick={() => setCurrentWeight("17 kg")}>
-                  17 kg
-                </StyledButton>
-              </StyledGridContainer>
-            </>
-          )}
+          <p>Amount of completed repetitions:</p>
+          <input type="range" min="0" max="30" value={currentReps} onChange={event => setCurrentReps(Number(event.target.value))}/>
+          <p>Applied weight:</p>
+          <input type="range" min="2" max="27" step="2.5" value={currentWeight} onChange={event => setCurrentWeight(Number(event.target.value))}/>
         </>
       )}
     </StyledExercise>
