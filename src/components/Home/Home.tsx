a39bc685044725d12e4e8f7963d52267236bc548
untypedjay <jay.developes@simplelogin.fr>
2022-01-07 21:01:40 +0100

    Update styled components in Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,13 @@
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 import { FaChartBar, FaCog } from 'react-icons/fa';
-import styled from "styled-components";
+import styled from 'styled-components';
 
-const StyledSettingsButton = styled.button`
+const StyledButton = styled.button`
   display: absolute;
   right: 0;
 `;
 
-const StyledImage = styled.img`
+const StyledInvisibleImage = styled.img`
   display: none;
 `;
 
@@ -20,17 +20,17 @@ export default function Home({ showVisitors = false }: Props) {
   
   return (
     <>
-      <StyledSettingsButton onClick={() => history.push("/settings")}><FaCog /></StyledSettingsButton>
-      <StyledSettingsButton onClick={() => history.push("/statistics")}><FaChartBar /></StyledSettingsButton>
+      <StyledButton onClick={() => history.push('/settings')}><FaCog /></StyledButton>
+      <StyledButton onClick={() => history.push('/statistics')}><FaChartBar /></StyledButton>
       <h1>WorkItOut</h1>
-      <button onClick={() => history.push("/workouts/0")}>
+      <button onClick={() => history.push('/workouts/0')}>
         <h3>Full Body Dumbbell</h3>
         <p>60 min</p>
       </button>
 
       { showVisitors ?
         <img src="https://profile-counter.glitch.me/great-app-sdkjsd-dev-2021-10-05/count.svg" alt="Visitors"/> :
-        <StyledImage src="https://profile-counter.glitch.me/great-app-sdkjsd-dev-2021-10-05/count.svg" alt="Visitors" />
+        <StyledInvisibleImage src="https://profile-counter.glitch.me/great-app-sdkjsd-dev-2021-10-05/count.svg" alt="Visitors" />
       }
     </>
   );
